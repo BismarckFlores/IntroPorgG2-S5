1e6db43f811038e7048f79850b9d66afcbccf893
Bismarck Flores <bjflores@uamv.edu.ni>
2025-04-22 07:47:48 -0600

    Se realizo un formato a la salida

--- actividad3/ejercicio1.py
+++ actividad3/ejercicio1.py
@@ -6,7 +6,7 @@ suma = calificacion1 + calificacion2 + calificacion3
 
 promedio = suma / 3
 
-print(f"""Calificacion 1: {calificacion1}
-      Calificacion 2: {calificacion2}
-      Calificacion 3: {calificacion3}
-      Promedio: {promedio: .2f}""")
+print(f"""Calificacion 1: {calificacion1:>3}
+Calificacion 2: {calificacion2:>3}
+Calificacion 3: {calificacion3:>3}
+{"Promedio:":<15} {promedio:>3.0f}""")
